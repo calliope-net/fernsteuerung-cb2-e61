6517c51eb9fb6b61cdf4f6d80a322798c4d0185e
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-08-18 11:15:58 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -10,6 +10,7 @@ input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
     cb2.fahreStrecke(1, 16, 20)
 })
 cb2.onAbstandEvent(function (abstand_Sensor, abstand_Stop, cm) {
+    cb2.buffer_Hindernis_ausweichen(btf.btf_receivedBuffer19(), false)
     cb2.event_Hindernis_ausweichen(
     Ultraschall_Sensor_Knopf_A && !(Spur_Sensor_Knopf_B),
     abstand_Stop,
@@ -29,7 +30,16 @@ input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     Spur_Sensor_Knopf_B = !(Spur_Sensor_Knopf_B)
     Ultraschall_Sensor_Knopf_A = Spur_Sensor_Knopf_B
 })
+btf.onReceivedDataChanged(function (receivedData, changed) {
+    cb2.fahreJoystick(receivedData, 50)
+    cb2.fahrplanBuffer5Strecken(btf.btf_receivedBuffer19(), btf.e3aktiviert.m1)
+    btf.setLedColors(btf.btf_RgbLed(btf.eRgbLed.a), 0x0000ff, true, true)
+    btf.zeige5x5Buffer(receivedData)
+    btf.zeige5x5Joystick(receivedData)
+    pins.pinDigitalWrite(pins.pins_eDigitalPins(pins.eDigitalPins.C16), !(btf.getSchalter(receivedData, btf.e0Schalter.b0)))
+})
 cb2.onSpurEvent(function (links_hell, rechts_hell, abstand_Stop) {
+    cb2.buffer_Spur_folgen(btf.btf_receivedBuffer19(), false, false, false)
     cb2.event_Spur_folgen(
     Spur_Sensor_Knopf_B,
     links_hell,
@@ -45,10 +55,21 @@ cb2.onSpurEvent(function (links_hell, rechts_hell, abstand_Stop) {
 let Spur_Sensor_Knopf_B = false
 let Ultraschall_Sensor_Knopf_A = false
 btf.comment(btf.btf_text("Erweiterung: calliope-net/fernsteuerung"))
-cb2.writeReset()
+cb2.beimStart()
 btf.zeigeBIN(cb2.readVersionArray()[1], btf.ePlot.bin, 2)
 btf.zeigeBIN(cb2.readSpannung(), btf.ePlot.bcd, 4)
 basic.forever(function () {
+    cb2.raiseBufferEvents(btf.btf_receivedBuffer19())
     cb2.raiseAbstandEvent(Ultraschall_Sensor_Knopf_A, 30, 35)
     cb2.raiseSpurEvent(Spur_Sensor_Knopf_B)
+})
+loops.everyInterval(700, function () {
+    if (btf.timeout(30000, true)) {
+        cb2.writeMotorenStop()
+        control.reset()
+    } else if (btf.timeout(1000)) {
+        btf.setLedColors(btf.btf_RgbLed(btf.eRgbLed.a), 0xff0000, true, true)
+    } else if (btf.timeout(1000, true)) {
+        btf.setLedColors(btf.btf_RgbLed(btf.eRgbLed.a), 0x00ff00)
+    }
 })
